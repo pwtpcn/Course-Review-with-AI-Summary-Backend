793ab7906843a993912821234443aaae1d8d3517
Thanapoom.C <palmdz08@gmail.com>
2026-01-04 14:48:13 +0700

    feat: working on user service

--- src/controller/user_controllers.ts
+++ src/controller/user_controllers.ts
@@ -10,6 +10,7 @@ export const userController = new Elysia({
   prefix: "/user",
   detail: { tags: ["User"] },
 })
+
   .post(
     "/register",
     async ({ body: { username, email, password } }) => {
@@ -86,4 +87,78 @@ export const userController = new Elysia({
         summary: "Login a user",
       },
     }
-  );
+  )
+
+  .get(
+    "/getall",
+    async () => {
+      return { users: await service.getAllUsers() };
+    },
+    {
+      detail: {
+        description: "Get all users",
+        summary: "Get all users",
+      },
+    }
+  )
+
+  .get(
+    "/getbyid/:id",
+    async ({ params: { id } }) => {
+      return { user: await service.getUserById(id) };
+    },
+    {
+      detail: {
+        description: "Get a user by id",
+        summary: "Get a user by id",
+      },
+    }
+  )
+
+  .post(
+    "/changeUsername",
+    async ({ body: { id, username } }) => {
+      const response = await service.changeUsername(id, username);
+      return { user: response };
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+        username: t.String({
+          minLength: 3,
+          maxLength: 20,
+        }),
+      }),
+      detail: {
+        description: "Change username of a user",
+        summary: "Change username of a user",
+      },
+    }
+  )
+
+  .post(
+    "/changePassword",
+    async ({ body: { id, password } }) => {
+      const response = await service.changePassword(id, password);
+      return { user: response };
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+        password: t.String({
+          format: "regex",
+          regex:
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+          message:
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+          minLength: 8,
+          maxLength: 15,
+        }),
+      }),
+      detail: {
+        description: "Change password of a user",
+        summary: "Change password of a user",
+      },
+    }
+  )
+    
--- src/service/user_services.ts
+++ src/service/user_services.ts
@@ -1,6 +1,7 @@
 import { User } from "../schema/user";
 import { dataSource } from "../data-source";
 import { DataSource } from "typeorm";
+import { hashPassword } from "../util/hash_password";
 
 class UserServices {
   private dataSource: DataSource;
@@ -14,6 +15,23 @@ class UserServices {
 
   async getUserByEmail(email: string) {
     return this.dataSource.manager.findOne(User, { where: { email } });
+  }
+
+  async getUserById(id: string) {
+    return this.dataSource.manager.findOne(User, { where: { id } });
+  }
+
+  async getAllUsers() {
+    return this.dataSource.manager.find(User);
+  }
+
+  async changeUsername(id: string, username: string) {
+    return this.dataSource.manager.update(User, id, { username });
+  }
+
+  async changePassword(id: string, password: string) {
+    const { hashedPassword, salt } = await hashPassword(password);
+    return this.dataSource.manager.update(User, id, { hashedPassword, salt });
   }
 }
 
