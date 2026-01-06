f6f4e84669f9e7a75665104d3ba06d4b1a528d2e
Thanapoom.C <palmdz08@gmail.com>
2026-01-06 15:20:03 +0700

    feat: edit index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { Elysia } from "elysia";
 import { dataSource } from "./data-source";
 import { userController } from "./controller/user_controllers";
-import {swagger} from "@elysiajs/swagger";
+import { courseController } from "./controller/course_controllers";
+import { teacherController } from "./controller/teacher_controller";
+import { swagger } from "@elysiajs/swagger";
 
 await dataSource.initialize();
 
-const app = new Elysia().use(swagger()).use(userController).listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .use(userController)
+  .use(courseController)
+  .use(teacherController)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
